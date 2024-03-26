fb6c617d3f7f3eac61fa0f09d2044181341044e3
Konstantin Alexeev <konstimac@Konstantins-MacBook-Air.local>
2024-03-26 13:19:31 -0400

    unstaged files

--- config.py
+++
@@ -1,5 +0,0 @@
-EMAIL_ADDRESS = "pierce342012@gmail.com"
-EMAIL_APP_PASSWORD = "gclm lpne ojll ukqp"
-RECIPIENT1 = "d.shehata@outlook.com"
-RECIPIENT2 = "kalexeev@outlook.com"
-BOTH_RECIPIENTS = [RECIPIENT1, RECIPIENT2]
